a2043574c8140606c68a83e5cb094a71e61485eb
Igor Panasiuk <ihor.panasiuk@nixsolutions.com>
2018-03-05 11:06:22 +0200

    First commit

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace MongoORM.Extensions
+{
+    internal static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Concat<T>(this IEnumerable<IEnumerable<T>> sequence)
+        {
+            IEnumerable<T> result = null;
+
+            foreach (var seq in sequence)
+            {
+                if (result == null)
+                {
+                    result = seq;
+                    continue;
+                }
+
+                result = result.Concat(seq);
+            }
+
+            return result;
+        }
+    }
+}
